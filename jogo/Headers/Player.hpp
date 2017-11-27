0bc09234d66433503086baff4c26c29bd717b739
NatiLucca <natielelucca@gmail.com>
2017-11-27 16:25:05 -0200

    Nave

---
+++ jogo/Headers/Player.hpp
@@ -0,0 +1,19 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Object.h"
+
+class Player(){
+      private:
+          Object nave;
+          float x;
+          float y;
+          float z;
+          float largura;
+          float altura;
+          float profundidade;
+          
+
+
+
+}
