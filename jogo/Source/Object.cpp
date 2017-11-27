a678918ee4c0c734e08e3ebe4cb36ca084972add
Guilherme <guinbsa@gmail.com>
2017-11-27 15:49:10 -0200

    ...

--- jogo/Source/Object.cpp
+++ jogo/Source/Object.cpp
@@ -29,31 +29,31 @@ void Object::addFace(Face face){
 
 void Object::glObject(){
     for(int i = 0; i < faces.size(); i++){
-        faces[i].glFace(comTextura, texture);
+        faces[i].glFace(comTextura, texture);//mostra o obj
     }
 }
 
 void Object::readObject(char *nomeArquivo){
     ifstream file;
     file.open(nomeArquivo,ios::in);
-    
+
     vector<Ponto> pontos;
     vector<Ponto> normais;
     vector<Ponto> texturas;
-    
+
     if (!file){
         cout << "Problemas na leitura do arquivo" << endl;
     }else{
-    
+
         string line;
-    
+
         char type;
         float a = 0,b = 0,c = 0;
         int aresta;
-    
+
         do{
-        
-            
+
+
             vector<Aresta> arestas;
             Face face;
             stringstream sline;
@@ -61,42 +61,42 @@ void Object::readObject(char *nomeArquivo){
             sline << line;
             sline >> type;
             if(type == 'v'){
-        
+
                 sline >> a >> b >> c;
                 if(abs(c) > minZ){
                     minZ = abs(c);
                 }
                 Ponto p = Ponto(a,b,c);
                 pontos.push_back(p);
-            
+
             }else if(type == 'f'){
-            
+
                 vector<int> as;
                 do{
                     sline >> aresta;
                     as.push_back(aresta);
                 }while(sline);
-                
+
                 for(int i = 0; i < as.size();i++){
-                
+
                     if(i != as.size()-1){
                         arestas.push_back( Aresta(pontos[as[i]-1], pontos[as[i+1]-1]) );
                     } else {
                         arestas.push_back( Aresta(pontos[as[i]-1], pontos[as[0]-1]));
-                        
+
                     }
                 }
-                
+
                 for(int i = 0; i < arestas.size();i++){
                     face.addAresta(arestas[i]);
                 }
                 this->addFace(face);
-           
+
             }
-        
+
         }while(file);
         faces.pop_back();
-    
+
     }
-    
+
 }
